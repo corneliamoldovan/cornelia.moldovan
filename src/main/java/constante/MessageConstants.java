1abb2fc066656a994644288e8849794d62f33b7b
corneliamoldovan <42037757+corneliamoldovan@users.noreply.github.com>
2018-08-02 17:10:46 +0300

    Delete MessageConstants.java

--- src/main/java/constante/MessageConstants.java
+++
@@ -1,8 +0,0 @@
-package constante;
-
-public class MessageConstants {
-	public static final String INVALIDEMAIL="There is 1 error\nInvalid email address";
-	public static final String INVALIDPASS="There is 1 error\nInvalid password.";
-
-
-}
